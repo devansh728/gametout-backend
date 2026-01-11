4376dbc046a5ea2b8e7370f71df8042ea391804d
devansh728 <gargdevansh728@gmail.com>
2026-01-12 03:29:49 +0530

    fixed embedded tomacat issue

--- src/main/java/com/gametout/gametout/GametoutApplication.java
+++ src/main/java/com/gametout/gametout/GametoutApplication.java
@@ -1,13 +1,16 @@
 package com.gametout.gametout;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class GametoutApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GametoutApplication.class, args);
-	}
+        new SpringApplicationBuilder(GametoutApplication.class)
+            .web(WebApplicationType.SERVLET)
+            .run(args);
+    }
 
 }
